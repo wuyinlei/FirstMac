6512db0499f9f0f3ce2bdda4bddc8a70a5caecac
wuyinlei <wuyinlei19931118@hotmail.com>
2016-09-29 22:04:33 +0800

    没什么实质性更新

--- app/src/main/java/yinlei/com/firstmac/MainActivity.java
+++ app/src/main/java/yinlei/com/firstmac/MainActivity.java
@@ -2,12 +2,22 @@ package yinlei.com.firstmac;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.hello).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.hello){
+            Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        }
     }
 }
